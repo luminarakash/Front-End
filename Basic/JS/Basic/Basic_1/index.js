a29e3d01be4abd88be296ebd532af6e2ffe648bf
Akash Patel <117297200+luminarakash@users.noreply.github.com>
2024-11-02 09:05:27 +0530

    Add files via upload

---
+++ Basic/JS/Basic/Basic_1/index.js
@@ -0,0 +1,59 @@
+//let a = true;
+//let a = 5;
+let a = 'Love You Bro kaise ho';
+console.log(a);
+
+console.log('Akash');
+
+const n = 6;
+//n = 6;
+console.log(n);
+ 
+let lastName='Akku';
+console.log(lastName);
+
+lastName = 4;
+console.log(lastName);
+
+//comparison op
+console.log(5>3);
+console.log(5<3);
+
+//ternary op
+let age=17;
+let status = (age >= 18) ? 'yes vote' : 'No vote';
+console.log(status);
+
+//control-> else-if
+let marks = 28;
+if(marks >= 90){
+    console.log('A');
+}
+else if(marks >= 80){
+    console.log('B');
+}
+else if(marks >= 70){
+    console.log('C');
+}
+else if(marks >= 60){
+    console.log('D');
+}
+else{
+    console.log('E');
+}
+
+//control -> switch
+let num = 5;
+switch(num){
+    case 1: console.log('A');
+    break;
+    case 2: console.log('B');
+    break;
+    case 3: console.log('C');
+    break;
+    case 4: console.log('D');
+    break;
+    default: console.log('k');
+}
+
+// Loops
